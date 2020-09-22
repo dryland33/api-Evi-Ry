37f2953fdb124c850009057d17baa53bd8de074c
Rylan Wassem <dryland28@gmail.com>
2020-09-21 20:59:55 -0400

    something that actually  works

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
-let maxResults = 10;
-
-function gitNews() {
-  const searchURL = 'https://api.github.com/users/dryland33/repos';
+function gitRepos(handle) {
+  const searchURL = 'https://api.github.com/users/' + handle + '/repos';
   fetch(searchURL)
     .then(response => {
       if (response.ok) {
@@ -17,70 +15,29 @@ function gitNews() {
     });
 }
 
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-  return queryItems.join('&');
-}
-
 function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
   $('#results-list').empty();
   // iterate through the results array, stopping at the max number of results
-  for (let i=0; i<maxResults ; i++){
+  for (let i = 0; i < responseJson.length; i++) {
     $('#results-list').append(
       `<li> 
         ${responseJson[i].name}
       </li>`
-    )}
+    )
+  }
   //display the results section  
   $('#results').removeClass('hidden');
-};
+}
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    //const searchTerm = $('#js-search-term').val();
-    //const maxResults = $('#js-max-results').val();
-    //gitNews(searchTerm, maxResults);
-    gitNews();
+    const searchTerm = $('#js-search-term').val();
+    //const searchTerm = document.getElementById('#js-search-term').value;
+    gitRepos(searchTerm);
   });
 }
 
-$(gitNews);
-
-/*
-
-function getNews(query, maxResults=10) {
-  const params = {
-    q: query,
-    pageSize: maxResults
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-
-  console.log(url);
-
-  const options = {
-    headers: new Headers({
-      "x-rapidapi-key": apiKey})
-  };
-
-  fetch(url, options)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson, maxResults))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-}
-
-
-
 $(watchForm);
-*/
